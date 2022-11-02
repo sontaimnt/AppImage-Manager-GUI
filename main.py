066c75f3be88c240d691f765c2475695d150ed6c
Abhiraj Rik <97382691+average-python-coder@users.noreply.github.com>
2022-11-02 22:25:33 +0530

    Added install dialog and online install dialog in progress..

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from os import system , chdir , getlogin
 from validators import url
 from requests import get
 
-prcsexit=None
-
 user=getlogin()
 
 root=Tk()
@@ -22,6 +20,8 @@ canvas.create_window(150 , 45, window=button1)
 button2=Button(root , text='Uninstall')
 canvas.create_window(250 , 45, window=button2)
 
+prcsexit=None
+
 class Install():
     def from_path(file):
         pathexists=isfile(file)
@@ -34,23 +34,54 @@ class Install():
         global prcsexit
         urlistrue=url(link)
         if urlistrue==True:
-            chdir('/home/{user}/appimage-manager')
+            chdir(f'/home/{user}/appimage-manager')
             file=get(link)
             appimage=open(filename , 'wb')
             appimage.write(file.content)
+            system(f"chmod +x {filename}")
             prcsexit=int(0)
         else:
-            global prcsexit
             return ValueError;prcsexit==int(1)
 
+class Install_Dialog():
+    def __init__():
+        global GUI
+        GUI=Tk()
+        GUI.title('Installation Mode:-')
+        GUI.geometry('300x150')
 
+        Install_Dialog.elements_load()            
+        GUI.mainloop()
+    
+    def elements_load():
+        global canvasdb , buttononline , buttonoffline
+        canvasdb=Canvas(GUI , width=300 , height=150)
+        canvasdb.pack()
+        canvasdb.create_window(150 , 30, window=Label(canvasdb , text="Select Install Mode:-"))
+
+        buttononline=Button(GUI , text='Online Install')
+        buttonoffline=Button(GUI , text='Offline Install')
+        canvasdb.create_window(150 , 75, window=buttononline)
+        canvasdb.create_window(150 , 115, window=buttonoffline)
+
+        buttononline.configure(command=InstallType_dialogs.online)
+
+class InstallType_dialogs():
+    def online():
+        global onlinedb
+        GUI.destroy()
+        onlinedb=Tk()
+        onlinedb.title('Installing Appimage:-')
+        onlinedb.mainloop()
 
 def check_on_startup():
-    direxists=isdir('appimage-manager')
-    if direxists:=True:
+    direxists=isdir(f'/home/{user}/appimage-manager')
+    if direxists==True:
         pass
     else:
         system('mkdir appimage-manager')
+
+button1.configure(command=Install_Dialog.__init__)
 
 check_on_startup()
 root.mainloop()
